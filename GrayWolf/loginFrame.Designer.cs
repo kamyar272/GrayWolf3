4905387fda360fbd8776b4b1877935100aa84e9f
mohsen momeni rad <mdmmomenirad@gmail.com>
2012-07-20 07:30:29 +0430

    minor problem! :)

--- GrayWolf/loginFrame.Designer.cs
+++ GrayWolf/loginFrame.Designer.cs
@@ -90,6 +90,7 @@
             this.button1.Text = "ورود";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.UseWaitCursor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox2
             // 
